0a9979c57667f0ccf383573f31717eddf433f795
Connor Bush <connormbush12@gmail.com>
2021-03-17 18:53:43 -0500

    Finished yelp app

--- src/screens/ResultsShowScreen.js
+++ src/screens/ResultsShowScreen.js
@@ -1,15 +1,44 @@
-import React from 'react'
-import {Text, View, StyleSheet} from 'react-native'
+import React, {useState, useEffect} from 'react'
+import {Text, View, FlatList, StyleSheet, Image} from 'react-native'
+import yelp from '../api/yelp'
 
 const ResultsShowScreen = ({navigation}) => {
+    const [result, setResult] = useState(null)
     const id = navigation.getParam('id')
-    console.log(id)
+
+    const getResult = async (id) => {
+        const response = await yelp.get(`/${id}`)
+        setResult(response.data)
+    }
+
+    useEffect(() => {
+        getResult(id)
+    }, [])
+
+    if(!result) {
+        return null
+    }
 
     return (
-        <Text>Results Show Screen</Text>
+        <View>
+        <Text>{result.name}</Text>
+        <FlatList 
+            data={result.photos} 
+            keyExtractor={(photo) => photo}
+            renderItem={({item}) => {
+                return <Image style={styles.image} source={{uri: item}} />
+            }} 
+        />
+        </View>
+
     )
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+    image: {
+        width: 300,
+        height: 200
+    }
+})
 
 export default ResultsShowScreen;
